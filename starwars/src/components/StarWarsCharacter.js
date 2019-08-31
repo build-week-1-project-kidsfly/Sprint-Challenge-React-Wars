af817c9f102ca807ba51c7f56c433406f70f2d02
josepaniagua <jpanjr@gmail.com>
2019-08-30 18:14:46 -0700

    starwarschracter

--- starwars/src/components/StarWarsCharacter.js
+++ starwars/src/components/StarWarsCharacter.js
@@ -1,1 +1,21 @@
-import
+import React from 'react';
+
+const StarWarsCharacter = (props) => {
+    const { name, height, mass, hair_color, skin_color } = props;
+    return (
+        <div>
+            <h1>{name}</h1>
+            <div>
+                <h4>`Height: ${height}`</h4>
+                <h4>`Mass: ${mass}kg`</h4>
+            </div>
+
+            <div>
+                <h4>`Hair Color: ${hair_color}`</h4>
+                <h4>`Skin Color: ${skin_color}`</h4>
+            </div>
+        </div>
+    );
+};
+
+export default StarWarsCharacter;
